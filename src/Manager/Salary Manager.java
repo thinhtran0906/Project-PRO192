4b8340af129e2beb79191a2bd4f705711a3c92aa
Tanluc24 <votanluc13579@gmail.com>
2026-06-25 20:11:22 +0700

    Update and rename Salary Manager to Salary Manager.java

--- src/Manager/Salary Manager.java
+++ src/Manager/Salary Manager.java
@@ -6,26 +6,28 @@ public class SalaryManager {
     Scanner sc = new Scanner(System.in);
 
     public void addSalary() {
-        System.out.print("Nhập lương: ");
+        System.out.print("Enter salary: ");
         salaries.add(sc.nextDouble());
     }
 
     public void displaySalaries() {
         for (int i = 0; i < salaries.size(); i++) {
-            System.out.println(i + ". " + salaries.get(i));
+            System.out.println("Salary " + i + ": " + salaries.get(i));
         }
     }
 
     public void updateSalary() {
-        System.out.print("Nhập vị trí: ");
+        System.out.print("Enter position: ");
         int i = sc.nextInt();
-        System.out.print("Nhập lương mới: ");
+
+        System.out.print("Enter new salary: ");
         salaries.set(i, sc.nextDouble());
     }
 
     public void deactivateSalary() {
-        System.out.print("Nhập vị trí: ");
+        System.out.print("Enter position: ");
         int i = sc.nextInt();
+
         salaries.set(i, 0.0);
     }
 }
