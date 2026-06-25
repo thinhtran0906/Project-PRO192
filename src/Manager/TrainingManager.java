bf507697da72d625057c30ec444732f70ff536d2
luongthanhnhan2k6-SE201403 <luongthanhnhan2k6@gmail.com>
2026-06-25 19:14:40 +0700

    Add files via upload

---
+++ src/Manager/TrainingManager.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class TrainingManager {
+
+    private ArrayList<TrainingSession> trainingList;
+
+    public TrainingManager() {
+        trainingList = new ArrayList<>();
+    }
+
+    public void addTrainingSession(
+            TrainingSession session) {
+
+        trainingList.add(session);
+    }
+
+    public void displayTrainingHistory() {
+
+        for (TrainingSession t : trainingList) {
+            System.out.println(t);
+        }
+    }
+}
