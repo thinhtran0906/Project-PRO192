d3f64413fd5516db71b14fc8f45c196e47300d08
Tanluc24 <votanluc13579@gmail.com>
2026-06-30 23:24:59 +0700

    Update SalaryManager.java

--- src/Manager/SalaryManager.java
+++ src/Manager/SalaryManager.java
@@ -1,17 +1,21 @@
+package Manager;
+
+import Entities.Player;
+
 public class SalaryManager {
-    public double calculateMonthlySalary(double baseSalary) {
-        return baseSalary;
-    }
-    public double calculatePerformanceBonus(double baseSalary, double bonusRate) {
-        return baseSalary * bonusRate;
+
+    // Calculate bonus
+    public double calculatePerformanceBonus(Player player, int points) {
+        return player.calculateBonus(points);
     }
 
-    
-     public boolean validateSalaryRules(double baseSalary) {
-        if (baseSalary > 0) {
-            return true;
-        } else {
-            return false;
-        }
+    // Calculate monthly salary
+    public double calculateMonthlySalary(Player player, int points) {
+        return player.getBaseSalary() + calculatePerformanceBonus(player, points);
+    }
+
+    // Validate salary
+    public boolean validateSalaryRules(Player player) {
+        return player.getBaseSalary() > 0;
     }
 }
